bc2c33d75a1ba54a83082bf17b1a562a75d025c3
Joseph Yaworski <joseph.yaworski@fireeye.com>
2016-05-27 10:18:02 -0400

    Add default_facts to spec_helper.rb. Closes #167

--- moduleroot/spec/spec_helper.rb
+++ moduleroot/spec/spec_helper.rb
@@ -1,6 +1,14 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 require 'rspec-puppet-facts'
 include RspecPuppetFacts
+
+RSpec.configure do |c|
+  c.default_facts = {
+    <%- @configs['default_facts'].each do |k, v| -%>
+    <%= k %>: <%= v %>
+    <%- end unless @configs['default_facts'].empty? -%>
+  }
+end
 <%- [@configs['spec_overrides']].flatten.compact.each do |line| -%>
  <%= line %>
 <%- end -%>
