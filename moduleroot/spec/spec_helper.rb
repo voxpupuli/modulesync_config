fc320c04a98370790c1d1e23346ce128ee3b8c2c
Tim Meusel <tim@bastelfreak.de>
2016-07-29 17:30:26 +0200

    add ability to parse facts file
    this way we can directly parse a yaml file. This is cleaner than the
    current approach:
    * writing the facts as yaml in module_defaults.yml
    * parse that and write as hash int spec_helper.rb
    
    this now allows us to share a default_facts.yml via modulesync + a
    modulespecific default_module_facts.yml.

--- moduleroot/spec/spec_helper.rb
+++ moduleroot/spec/spec_helper.rb
@@ -3,11 +3,13 @@ require 'rspec-puppet-facts'
 include RspecPuppetFacts
 
 RSpec.configure do |c|
-  c.default_facts = {
-    <%- @configs['default_facts'].each do |k, v| -%>
-    <%= k %>: <%= v %>,
-    <%- end unless @configs['default_facts'].empty? -%>
+  default_facts = {
+    puppetversion: Puppet.version,
+    facterversion: Facter.version
   }
+  default_facts += YAML.read_file('default_facts.yml') if File.exist?('default_facts.yml')
+  default_facts += YAML.read_file('default_facts.yml') if File.exist?('default_module_facts.yml')
+  c.default_facts = default_facts
 end
 
 <%- [@configs['spec_overrides']].flatten.compact.each do |line| -%>
