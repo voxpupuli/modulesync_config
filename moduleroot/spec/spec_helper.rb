f78a1494a483ab78404bc18ed065d91d64569ee1
Alexander Fisher <alex@linfratech.co.uk>
2016-11-16 13:55:58 +0000

    Revert "Fix SimpleCove filter"
    This reverts commit 152e345be5669b598cbb3d26d8ddbffd88d4a7b4.

--- moduleroot/spec/spec_helper.rb
+++ moduleroot/spec/spec_helper.rb
@@ -6,14 +6,13 @@ unless RUBY_VERSION =~ %r{^1.9}
   require 'coveralls'
   require 'simplecov'
   require 'simplecov-console'
-
   SimpleCov.formatters = [
     SimpleCov::Formatter::HTMLFormatter,
     SimpleCov::Formatter::Console,
     Coveralls::SimpleCov::Formatter
   ]
   SimpleCov.start do
-   add_filter 'spec/fixtures'
+    add_filter '/spec'
   end
 end
 
