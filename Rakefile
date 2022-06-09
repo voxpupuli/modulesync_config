voxpupuli/modulesync_config

commit 9fb2f67c3d80c363106f9d6db718ff8b00f05814

Release 4.2.0

Rakefile
@@ -5,3 +5,16 @@ task :metadata_deps do
   files = FileList['modules/*/*/metadata.json']
   MetadataJsonDeps.run(files)
 end
+
+begin
+  require 'github_changelog_generator/task'
+
+  GitHubChangelogGenerator::RakeTask.new :changelog do |config|
+    config.header = "# Changelog\n\nAll notable changes to this project will be documented in this file."
+    config.exclude_labels = %w{duplicate question invalid wontfix wont-fix skip-changelog}
+    config.user = 'voxpupuli'
+    config.project = 'modulesync_config'
+    config.future_release = YAML.safe_load(File.read('moduleroot/.msync.yml.erb'))['modulesync_config_version']
+  end
+rescue LoadError
+end

moduleroot/.msync.yml.erb
@@ -2,4 +2,4 @@
 # Managed by modulesync - DO NOT EDIT
 # https://voxpupuli.org/docs/updating-files-managed-with-modulesync/
 
-modulesync_config_version: '4.1.0'
+modulesync_config_version: '4.2.0'

commit e7c2b00ffc3bbcfaa8dde0e75c62210768b8a93e

Explicitly require YAML in Rakefile
Previously it was relying on other gems to pull in YAML while using it a
bit lower. This makes it explicit which at worst is redundant and at
best prevents failures. It is loaded after github_changelog_generator so
that if the gem isn't present, YAML isn't pulled in either.

Fixes: 9fb2f67c3d80c363106f9d6db718ff8b00f05814

Rakefile
@@ -8,6 +8,7 @@ end
 
 begin
   require 'github_changelog_generator/task'
+  require 'yaml'
 
   GitHubChangelogGenerator::RakeTask.new :changelog do |config|
     config.header = "# Changelog\n\nAll notable changes to this project will be documented in this file."